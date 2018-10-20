b799599db143d9c6b59555c257582dc0142ac4b7
dehann <dehann.fourie@gmail.com>
2018-10-20 15:19:11 -0400

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Base: Test
+using Test
 using KernelDensityEstimatePlotting
 using IncrementalInference
 using RoME
--- test/testPose2Point2Plotting.jl
+++ test/testPose2Point2Plotting.jl
@@ -3,7 +3,7 @@
 using RoME, IncrementalInference, Distributions
 using KernelDensityEstimate
 using RoMEPlotting, Gadfly
-using Base: Test
+using Test
 
 begin
 
@@ -13,18 +13,16 @@ N = 75
 fg = initfg()
 
 
-initCov = diagm([0.03;0.03;0.001])
-odoCov = diagm([3.0;3.0;0.01])
+initCov = Matrix(Diagonal([0.03;0.03;0.001]))
+odoCov = Matrix(Diagonal([3.0;3.0;0.01]))
 
 # Some starting position
 v1 = addNode!(fg, :x0, Pose2, N=N)
-# v1 = addNode!(fg, :x0, zeros(3,1), diagm([1.0;1.0;0.1]), N=N)
 initPosePrior = PriorPose2(zeros(3,1), initCov, [1.0])
 f1  = addFactor!(fg,[v1], initPosePrior)
 
 # and a second pose
 v2 = addNode!(fg, :x1, Pose2, N=N)
-# v2 = addNode!(fg, :x1, vectoarr2([50.0;0.0;pi/2]), diagm([1.0;1.0;0.05]), N=N)
 ppc = Pose2Pose2(MvNormal([50.0;0.0;pi/2], odoCov))
 f2 = addFactor!(fg, [v1;v2], ppc)
 
@@ -38,13 +36,13 @@ inferOverTreeR!(fg, tree,N=N)
 # inferOverTree!(fg, tree, N=N)
 
 # check that yaw is working
-v3 = addNode!(fg, :x2, zeros(3,1), diagm([1.0;1.0;0.05]), N=N)
+v3 = addNode!(fg, :x2, Pose2, N=N)
 ppc = Pose2Pose2(MvNormal([50.0;0.0;0.0], odoCov))
 f3 = addFactor!(fg, [v2;v3], ppc)
 
 
 # new landmark
-l1 = addNode!(fg, :l1, zeros(2,1), diagm([1.0;1.0]), N=N)
+l1 = addNode!(fg, :l1, Point2, N=N)
 # and pose to landmark constraint
 rhoZ1 = norm([10.0;0.0])
 ppr = Pose2Point2BearingRange{Uniform, Normal}(Uniform(-pi,pi),Normal(rhoZ1,1.0))
@@ -52,7 +50,7 @@ f4 = addFactor!(fg, [v1;l1], ppr)
 
 
 # add a prior to landmark
-pp2 = PriorPoint2D([10.0;0.0], diagm([1.0;1.0]), [1.0])
+pp2 = PriorPoint2D([10.0;0.0], Matrix(Diagonal([1.0;1.0])), [1.0])
 
 f5 = addFactor!(fg,[l1], pp2)
 
