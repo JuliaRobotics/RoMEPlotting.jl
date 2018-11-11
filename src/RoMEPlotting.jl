832d2bb18afbe31162e65953825276dcc785765b
dehann <dehann.fourie@gmail.com>
2018-11-10 23:14:53 -0500

    comment

--- src/RoMEPlotting.jl
+++ src/RoMEPlotting.jl
@@ -18,9 +18,9 @@ import Gadfly: plot
 
 # TODO temporary fix for Compose based plotting in Julia 0.7 (Oct 2018)
 # see
-@warn "[TEMPORARY WORKAROUND, pangolayout] for plotting with Compose and Gadfly.jl, see https://github.com/GiovineItalia/Gadfly.jl/issues/1206"
-import Compose: pangolayout
-const pangolayout = PangoLayout()
+# @warn "[TEMPORARY WORKAROUND, pangolayout] for plotting with Compose and Gadfly.jl, see https://github.com/GiovineItalia/Gadfly.jl/issues/1206"
+# import Compose: pangolayout
+# const pangolayout = PangoLayout()
 
 export
   # Associated with IncrementalInference
