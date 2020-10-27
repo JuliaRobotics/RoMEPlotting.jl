2de89480712e406b51aed5c1835a06b89e8a351c
dehann <dehann.fourie@gmail.com>
2020-10-27 17:14:05 -0400

    compat and flux fix

--- src/FluxSpecificFeatures.jl
+++ src/FluxSpecificFeatures.jl
@@ -1,9 +1,9 @@
 # features specifically related to Flux.jl
 @info "RoMEPlotting is adding Flux related functionality."
 
-@eval PlotTypesPose2 = Union{Type{Pose2Pose2}, Type{Pose2Point2BearingRange}, Type{Pose2Point2Range}, Type{Pose2Point2Bearing}, Type{FluxModelsPose2Pose2}}
+@eval PlotTypesPose2 = Union{Type{Pose2Pose2}, Type{Pose2Point2BearingRange}, Type{Pose2Point2Range}, Type{Pose2Point2Bearing}, Type{MixtureFluxPose2Pose2}}
 
-@eval ExtendedPose2Pose2Types = Union{Pose2Pose2, FluxModelsPose2Pose2}
+@eval ExtendedPose2Pose2Types = Union{Pose2Pose2, MixtureFluxPose2Pose2}
 
 # rerun since a few functions need to be reloaded with Flux
 include("PlotFactorsReload.jl")
