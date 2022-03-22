8f827a103574067c9d4cfcb5d2d36942df1d364b
dehann <dehann.fourie@gmail.com>
2022-03-22 13:14:39 -0700

    and bugfix

--- test/testPlotSLAMandPose.jl
+++ test/testPlotSLAMandPose.jl
@@ -28,7 +28,7 @@ plotSLAM2D(fg, drawhist=true, drawPoints=true, drawContour=true, drawEllipse=fal
 plotSLAM2D(fg, drawhist=true, drawPoints=true, drawContour=true, drawEllipse=true)
 
 
-plotSLAM2D(fg; variableList=[:x1;x5;:l1])
+plotSLAM2D(fg; variableList=[:x1;:x5;:l1])
 
 # plot runs but doesnt draw the levels at time of wrting the test.  Good to test the API regardless.
 plotSLAM2D(fg, drawhist=false, drawPoints=true, drawContour=true, drawEllipse=false, levels=5)
